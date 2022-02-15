95c9f54c909bd6efc40cf63e054b84b710c4588e
cezar.vant <cezar.vant@123fombuilder.com>
2022-02-15 16:00:07 +0200

    WIP on feature/0001

--- src/Framework/Providers/RoutingServiceProvider.php
+++ src/Framework/Providers/RoutingServiceProvider.php
@@ -7,9 +7,10 @@ use Chassis\Framework\Routers\Router;
 use Chassis\Framework\Routers\RouterInterface;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
 use function Chassis\Helpers\app;
 
-abstract class RoutingServiceProvider extends AbstractServiceProvider
+abstract class RoutingServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
 {
     protected array $routes = [];
 
@@ -18,8 +19,17 @@ abstract class RoutingServiceProvider extends AbstractServiceProvider
         return $id === RouterInterface::class;
     }
 
+    public function boot(): void
+    {
+        var_dump([__METHOD__, $this->routes]);
+        sleep(1);
+    }
+
     public function register(): void
     {
+        var_dump([__METHOD__, $this->routes]);
+        sleep(1);
+
         $this->getContainer()
             ->add(RouterInterface::class, Router::class)
             ->addArguments([
