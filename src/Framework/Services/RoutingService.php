74db9e2f75df5666a9955d35c4a180f49df529b2
cezar.vant <cezar.vant@123fombuilder.com>
2022-02-16 10:11:39 +0200

    WIP on feature/0001 - add response flow without publisher

--- src/Framework/Services/RoutingService.php
+++ src/Framework/Services/RoutingService.php
@@ -5,22 +5,19 @@ declare(strict_types=1);
 namespace Chassis\Framework\Services;
 
 use Chassis\Framework\Brokers\Amqp\BrokerResponse;
-use Chassis\Framework\Brokers\Amqp\MessageBags\MessageBagInterface;
 
 class RoutingService extends BrokerAbstractService
 {
     /**
-     * @param MessageBagInterface $messageBag
-     *
      * @return BrokerResponse|void
      */
-    public function routeNotfound(MessageBagInterface $messageBag)
+    public function routeNotfound()
     {
-        if (empty($messageBag->getProperty("reply_to"))) {
+        if (empty($this->messageBag->getProperty("reply_to"))) {
             return;
         }
         return $this->response()
-            ->fromContext($messageBag)
+            ->fromContext($this->messageBag)
             ->setStatus(404, "ROUTE NOT FOUND");
     }
 }
