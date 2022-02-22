aa29f862f7143223b1104e2bf76cc506a40b9927
cezar.vant <cezar.vant@123fombuilder.com>
2022-02-22 20:14:59 +0200

    Ctrl+C docker shutdown improvements - workers stop handler

--- src/Framework/Workers/Worker.php
+++ src/Framework/Workers/Worker.php
@@ -20,7 +20,7 @@ class Worker implements WorkerInterface
 {
     private const LOGGER_COMPONENT_PREFIX = "worker_";
     private const LOOP_EACH_MS = 50;
-    private const SUBSCRIBER_ITERATE_MAX_RETRY = 5;
+    private const SUBSCRIBER_ITERATE_MAX_RETRY = 50;
 
     private Application $application;
     private ChannelsInterface $channels;
