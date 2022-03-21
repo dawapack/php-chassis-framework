47f9fd6300377f152eaf515bffdb4a039b7c8a26
cezar.vant <cezar.vant@123fombuilder.com>
2022-03-21 18:53:03 +0200

    WIP on feature/0006

--- src/Framework/Bus/AMQP/Inbound/AMQPInboundBus.php
+++ src/Framework/Bus/AMQP/Inbound/AMQPInboundBus.php
@@ -89,12 +89,11 @@ class AMQPInboundBus implements AMQPInboundBusInterface
      */
     public function iterate(): void
     {
+
+        var_dump([__METHOD__, isset($this->amqpChannel)]);
+
         try {
-            if (!$this->amqpChannel->is_open()) {
-                usleep((int)(self::ITERATE_WAIT*1000000));
-            } else {
-                $this->amqpChannel->wait(null, false, self::ITERATE_WAIT);
-            }
+            $this->amqpChannel->wait(null, false, self::ITERATE_WAIT);
         } catch (AMQPTimeoutException $reason) {
             // fault-tolerant - this is a normal behaviour
         }
