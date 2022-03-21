5d2a0b22d56b2e7f25160c0d44936b3b7d2cae2b
cezar.vant <cezar.vant@123fombuilder.com>
2022-03-21 12:10:24 +0200

    WIP on feature/0006

--- src/Framework/Threads/ThreadInstance.php
+++ src/Framework/Threads/ThreadInstance.php
@@ -188,8 +188,8 @@ class ThreadInstance implements ThreadInstanceInterface
                     array $threadConfiguration,
                     Channel $workerChannel,
                     Channel $threadChannel,
-                    $inboundRouter,
-                    $outboundRouter
+                    $inboundRouter
+//                    $outboundRouter
                 ): void {
                     // Define application in Closure as worker
                     define('RUNNER_TYPE', 'worker');
@@ -265,7 +265,7 @@ class ThreadInstance implements ThreadInstanceInterface
                     $this->workerChannel,
                     $this->threadChannel,
                     app(InboundRouterInterface::class),
-                    app(OutboundRouterInterface::class)
+//                    app(OutboundRouterInterface::class)
                 ]
             );
         } catch (Throwable $reason) {
