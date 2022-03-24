f84a933b9d71be1f67d30fc9f5438aba99651b3b
cezar.vant <cezar.vant@123fombuilder.com>
2022-03-24 10:22:23 +0200

    WIP on feature/0006

--- src/Framework/Kernel.php
+++ src/Framework/Kernel.php
@@ -35,10 +35,16 @@ class Kernel implements KernelInterface
      */
     public function boot(): void
     {
-        if ($this->app->isWorker()) {
-            ($this->app->get(WorkerInterface::class))->start();
-        } elseif ($this->app->isDaemon()) {
-            ($this->app->get(ThreadsManagerInterface::class))->start($this->stopRequested);
+        try {
+
+            if ($this->app->isWorker()) {
+                ($this->app->get(WorkerInterface::class))->start();
+            } elseif ($this->app->isDaemon()) {
+                ($this->app->get(ThreadsManagerInterface::class))->start($this->stopRequested);
+            }
+
+        } catch (\Throwable $reason) {
+            var_dump($reason->getTraceAsString());
         }
     }
 
