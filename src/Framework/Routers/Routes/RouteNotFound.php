164f54d886c2cd193ba073059f5babcbb3df7645
cezar.vant <cezar.vant@123fombuilder.com>
2022-03-24 07:18:40 +0200

    WIP on feature/0006

--- src/Framework/Routers/Routes/RouteNotFound.php
+++ src/Framework/Routers/Routes/RouteNotFound.php
@@ -4,17 +4,37 @@ declare(strict_types=1);
 
 namespace Chassis\Framework\Routers\Routes;
 
+use Chassis\Application;
 use Chassis\Framework\Adapters\Message\InboundMessageInterface;
 use Chassis\Framework\Adapters\Message\OutboundMessageInterface;
 use DateTime;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
+
 use function Chassis\Helpers\app;
 
 class RouteNotFound
 {
+    protected const LOGGER_COMPONENT_PREFIX = "route_not_found";
+
     protected const DEFAULT_VERSION = '1.0.0';
     protected const DEFAULT_DATETIME_FORMAT = 'Y-m-d\TH:i:s.vP';
+    protected const DEFAULT_BODY = ["items" => [], "meta" => null];
+    protected const STATUS_CODE = 404;
+    protected const STATUS_MESSAGE = 'NOT FOUND';
+
+    private Application $application;
+
+    /**
+     * @param Application|null $application
+     *
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __construct(Application $application = null)
+    {
+        $this->application = is_null($application) ? app() : $application;
+    }
 
     /**
      * Nobody cares about the implementation
@@ -29,6 +49,14 @@ class RouteNotFound
     public function __invoke(InboundMessageInterface $message): ?OutboundMessageInterface
     {
         if (empty($message->getProperty("reply_to"))) {
+            $this->application->logger()->warning(
+                "Route not found",
+                [
+                    "component" => self::LOGGER_COMPONENT_PREFIX,
+                    "message_properties" => $message->getProperties(),
+                ]
+            );
+
             return null;
         }
 
@@ -36,13 +64,13 @@ class RouteNotFound
         $headers = [
             "version" => self::DEFAULT_VERSION,
             "dateTime" => (new DateTime())->format(self::DEFAULT_DATETIME_FORMAT),
-            "statusCode" => 404,
-            "statusMessage" => "NOT FOUND"
+            "statusCode" => self::STATUS_CODE,
+            "statusMessage" => self::STATUS_MESSAGE
         ];
 
-        return app(OutboundMessageInterface::class)
+        return $this->application->get(OutboundMessageInterface::class)
             ->setDefaultProperties()
             ->setHeaders($headers)
-            ->setBody([]);
+            ->setBody(self::DEFAULT_BODY);
     }
 }
